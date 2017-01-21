d8a63e7da54d05367446b424cdee9db6b691458c
Rick G <lacunainc04@gmail.com>
2017-01-21 10:51:20 -0800

    A markers

--- app/assets/javascripts/angular-app/controllers/HomeController.js
+++ app/assets/javascripts/angular-app/controllers/HomeController.js
@@ -1,9 +1,28 @@
 angular.module('rails-angular')
   .controller('HomeController', HomeController)
-  function HomeController(NgMap) {
+  function HomeController(NgMap, $http) {
     NgMap.getMap().then(function(map) {
       console.log(map.getCenter());
       console.log('markers', map.markers);
       console.log('shapes', map.shapes);
+
+      $http.get('http://localhost:3000/point_of_interest')
+        .then(function(res) {
+          var pois = res.data
+          var markers = []
+
+          pois.forEach(function(place) {
+            var pos = {lat: parseFloat(place.latitude), lng: parseFloat(place.longitude)}
+
+            var marker = new google.maps.Marker({
+              map: map,
+              position: pos,
+              title: place.name
+              // animation: google.maps.Animation.BOUNCE
+            })
+            markers.push(marker)
+          })
+        })
+
     });
   }
